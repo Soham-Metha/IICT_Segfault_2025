ba4833fe64feb0895d42c250fc984318bceabda8
Soham Metha <sohammetha01@gmail.com>
2025-09-02 02:56:39 +0530

    Frontend: Line.h: Cleanup

--- include/Frontend/Layer_Line.h
+++ include/Frontend/Layer_Line.h
@@ -4,47 +4,14 @@
 #include <Utils/strings.h>
 #include <Frontend/Layer_Statement.h>
 
-enum Line_Type { LINE_TYPE_DIRECTIVE, LINE_TYPE_FUNCTION, LINE_TYPE_COUNT };
-
-typedef struct Line_View Line_View;
-typedef struct Line_as_Directive Line_as_Directive;
-typedef struct Line_as_Function Line_as_Function;
 typedef struct CodeBlock CodeBlock;
-typedef struct Scope Scope;
 typedef struct StmtNode StmtNode;
-typedef union Line_Value Line_Value;
-typedef enum Line_Type Line_Type;
-
-struct Line_as_Directive {
-	String name;
-	String body;
-};
-
-struct Line_as_Function {
-	String name;
-	String arglist;
-};
-
-union Line_Value {
-	Line_as_Directive as_Directive;
-	Line_as_Function as_Function;
-};
-
-struct Line_View {
-	Line_Type type;
-	Line_Value value;
-};
 
 struct CodeBlock {
 	StmtNode *begin;
 	StmtNode *end;
 };
 
-struct Scope {
-	CodeBlock block;
-	Scope *prev;
-};
-
-String getNextLine(Scope *scope);
+CodeBlock getBlock();
 
 #endif
