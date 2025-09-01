8c2275d5d5e4e7be0886f405153143dac76a330d
Soham Metha <sohammetha01@gmail.com>
2025-09-01 08:57:20 +0000

    01/09/2025 : 14:27:20

--- src/Backend/Layer_File.c
+++ src/Backend/Layer_File.c
@@ -3,6 +3,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// ------------------------------------------------------------- HELPERS ---------------------------------------------------------------------
+
 File_View file;
 
 void readFile(const char *filePath)
