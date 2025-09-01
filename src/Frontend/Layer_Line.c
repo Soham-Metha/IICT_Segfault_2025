349d13bf433bf0a3e0f792162602bedbc57e18a6
Soham Metha <sohammetha01@gmail.com>
2025-09-01 13:35:14 +0000

    01/09/2025 : 19:05:14

--- src/Frontend/Layer_Line.c
+++ src/Frontend/Layer_Line.c
@@ -1,5 +1,6 @@
 #include <Frontend/Layer_Line.h>
 #include <Frontend/Layer_File.h>
+#include <Frontend/Layer_Statement.h>
 #include <Utils/strings.h>
 #include <Wrapper/IO.h>
 
@@ -23,27 +24,8 @@ String getNextLine()
 	} while (line.len == 0 && file.contents.len > 0);
 
 	getNextStmt(line);
-    while (line.len > 0) {
-        Line_View line_v = getNextStmt(line);
-        (void)line_v;
-    }
-}
-
-void getNextStmt(String line)
-{
-	Line_View result = { 0 };
-	if (starts_with(line, STR("#"))) {
-		split_str_by_len(&line, 1);
-		result.type = LINE_TYPE_DIRECTIVE;
-		result.value.as_Directive.name =
-			trim(split_str_by_delim(&line, ' '));
-		result.value.as_Directive.body = trim(line);
-		print(WIN_STDOUT,
-		      "\n[STMT] Identified Directive: '%.*s', body: '%.*s'",
-		      result.value.as_Directive.name.len,
-		      result.value.as_Directive.name.data,
-		      result.value.as_Directive.body.len,
-		      result.value.as_Directive.body.data);
+	while (line.len > 0) {
+		Line_View line_v = getNextStmt(line);
+		(void)line_v;
 	}
-	(void)result;
 }
