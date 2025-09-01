6e782041ea061da8c269a433bd5ec244ab242c03
Soham Metha <sohammetha01@gmail.com>
2025-09-01 15:47:58 +0000

    01/09/2025 : 21:17:58

--- include/Frontend/Layer_Statement.h
+++ include/Frontend/Layer_Statement.h
@@ -2,6 +2,46 @@
 #define STMT_LAYER_FRONTEND
 #include <Frontend/Layer_Line.h>
 #include <Utils/strings.h>
+#include <stdint.h>
+
+enum StmtType {
+	STMT_VARIABLE,
+	STMT_LIT_INT,
+	STMT_LIT_FLOAT,
+	STMT_LIT_CHAR,
+	STMT_LIT_STR,
+	STMT_FUNCALL,
+};
+
+typedef struct Stmt Stmt;
+typedef enum StmtType StmtType;
+typedef union StmtValue StmtValue;
+typedef struct Funcall Funcall;
+typedef struct FuncallArg FuncallArg;
+
+union StmtValue {
+	String as_var;
+	uint64_t as_int;
+	double as_float;
+	char as_char;
+	String as_str;
+	Funcall *as_funcall;
+};
+
+struct Stmt {
+	StmtType type;
+	StmtValue value;
+};
+
+struct FuncallArg {
+	FuncallArg *next;
+	Stmt value;
+};
+
+struct Funcall {
+	String name;
+	FuncallArg *args;
+};
 
 Line_View getNextStmt(String line);
 
