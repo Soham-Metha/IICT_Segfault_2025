c45dc499c1dbaf5d47dba814393fcc49905d7399
Soham Metha <sohammetha01@gmail.com>
2025-09-01 12:30:00 +0000

    01/09/2025 : 18:00:00

--- include/Frontend/Layer_Line.h
+++ include/Frontend/Layer_Line.h
@@ -1,8 +1,36 @@
 #ifndef LINE_LAYER_FRONTEND
 #define LINE_LAYER_FRONTEND
-
-#include <Utils/strings.h>
 #define COMMENT_SYMBOL '%'
+#include <Utils/strings.h>
+
+typedef struct Line_View Line_View;
+typedef struct Line_as_Directive Line_as_Directive;
+typedef struct Line_as_Function Line_as_Function;
+typedef union Line_Value Line_Value;
+typedef enum Line_Type Line_Type;
+
+enum Line_Type { LINE_TYPE_DIRECTIVE, LINE_TYPE_FUNCTION, LINE_TYPE_COUNT };
+
+struct Line_as_Directive {
+	String name;
+	String Body;
+};
+
+struct Line_as_Function {
+	String name;
+	String arglist;
+};
+
+union Line_Value {
+	Line_as_Directive as_Directive;
+	Line_as_Function as_Function;
+};
+
+struct Line_View {
+	Line_Type type;
+	Line_Value value;
+};
+
 String getNextLine();
 
 #endif
