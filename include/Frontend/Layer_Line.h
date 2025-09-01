fc88f0d930d87700d23b54426f37b5bfc4b003e9
Soham Metha <sohammetha01@gmail.com>
2025-09-01 13:04:13 +0000

    01/09/2025 : 18:34:13

--- include/Frontend/Layer_Line.h
+++ include/Frontend/Layer_Line.h
@@ -3,13 +3,13 @@
 #define COMMENT_SYMBOL '%'
 #include <Utils/strings.h>
 
+enum Line_Type { LINE_TYPE_DIRECTIVE, LINE_TYPE_FUNCTION, LINE_TYPE_COUNT };
+
 typedef struct Line_View Line_View;
 typedef struct Line_as_Directive Line_as_Directive;
 typedef struct Line_as_Function Line_as_Function;
 typedef union Line_Value Line_Value;
 typedef enum Line_Type Line_Type;
-
-enum Line_Type { LINE_TYPE_DIRECTIVE, LINE_TYPE_FUNCTION, LINE_TYPE_COUNT };
 
 struct Line_as_Directive {
 	String name;
