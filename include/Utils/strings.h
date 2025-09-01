a1b9b9692d510f13874c923a4387710fb6b34863
Soham Metha <sohammetha01@gmail.com>
2025-09-01 10:12:45 +0000

    01/09/2025 : 15:42:45

--- include/Utils/strings.h
+++ include/Utils/strings.h
@@ -1,5 +1,6 @@
 #ifndef STRINGS
 #define STRINGS
+#include <stdbool.h>
 
 typedef struct String String;
 
@@ -7,5 +8,21 @@ struct String {
 	unsigned int len;
 	const char *data;
 };
+
+String ltrim(String s);
+String rtrim(String s);
+String trim(String s);
+
+bool compare_str(String a, String b);
+
+bool get_index_of(String str, char c, size_t *index);
+
+String split_str_by_delim(String *s, char c);
+String split_str_by_condition(String *str, bool (*predicate)(char x));
+String split_str_by_len(String *str, size_t n);
+String split_str_by_len_reversed(String *str, size_t n);
+
+bool starts_with(String str, String prefix);
+bool ends_with(String str, String expected);
 
 #endif
