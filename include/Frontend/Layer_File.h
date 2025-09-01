fd90958606c4cb1b59d2832390d5f477f81c1ce3
Soham Metha <sohammetha01@gmail.com>
2025-09-01 18:09:27 +0000

    01/09/2025 : 23:39:27

--- include/Frontend/Layer_File.h
+++ include/Frontend/Layer_File.h
@@ -3,11 +3,17 @@
 #include <Utils/strings.h>
 
 typedef struct File_View File_View;
+typedef struct Scope Scope;
 
 struct File_View {
 	const char *file_path;
 	String contents;
 	unsigned int line_num;
+};
+
+struct Scope {
+	CodeBlock block;
+	Scope *prev;
 };
 
 extern File_View file;
