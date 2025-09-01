072de5bdf4615cc62f56e8856ae2951e8290ba7b
Soham Metha <sohammetha01@gmail.com>
2025-09-01 14:05:18 +0000

    01/09/2025 : 19:35:18

--- src/Frontend/Layer_Statement.c
+++ src/Frontend/Layer_Statement.c
@@ -5,18 +5,10 @@
 Line_View getNextStmt(String line)
 {
 	Line_View result = { 0 };
-	if (starts_with(line, STR("#"))) {
-		split_str_by_len(&line, 1);
-		result.type = LINE_TYPE_DIRECTIVE;
-		result.value.as_Directive.name =
-			trim(split_str_by_delim(&line, ' '));
-		result.value.as_Directive.body = trim(line);
-		print(WIN_STDOUT,
-		      "\n[STMT] Identified Directive: '%.*s', body: '%.*s'",
-		      result.value.as_Directive.name.len,
-		      result.value.as_Directive.name.data,
-		      result.value.as_Directive.body.len,
-		      result.value.as_Directive.body.data);
-	}
+	// Token tok;
+	// while (line.len > 0) {
+	// 	tok = getNextToken(&line);
+	// 	print("\n[STMT] token %.*s", tok.text.len, tok.text.data);
+	// }
 	return result;
 }
