fdc920381598f1ee644969768751b7c5101e36c7
Soham Metha <sohammetha01@gmail.com>
2025-09-01 14:14:23 +0000

    01/09/2025 : 19:44:23

--- src/Frontend/Layer_Statement.c
+++ src/Frontend/Layer_Statement.c
@@ -2,6 +2,36 @@
 #include <Frontend/Layer_Xpression.h>
 #include <Wrapper/IO.h>
 
+const char *getTokenName(TokenType type)
+{
+	switch (type) {
+	case TOKEN_TYPE_STR:
+		return "string";
+	case TOKEN_TYPE_CHAR:
+		return "character";
+	case TOKEN_TYPE_NUMBER:
+		return "number";
+	case TOKEN_TYPE_NAME:
+		return "name";
+	case TOKEN_TYPE_OPEN_PAREN:
+		return "open paren";
+	case TOKEN_TYPE_CLOSING_PAREN:
+		return "closing paren";
+	case TOKEN_TYPE_OPEN_CURLY:
+		return "open curly";
+	case TOKEN_TYPE_CLOSING_CURLY:
+		return "closing curly";
+	case TOKEN_TYPE_COMMA:
+		return "comma";
+	case TOKEN_TYPE_FUNC:
+		return "func";
+	default: {
+		assert(false && "getTokenName: unreachable");
+		exit(1);
+	}
+	}
+}
+
 Line_View getNextStmt(String line)
 {
 	Line_View result = { 0 };
@@ -10,8 +40,8 @@ Line_View getNextStmt(String line)
 	while (line.len > 0) {
 		line = trim(line);
 		tok = getNextToken(&line);
-		print(WIN_STDOUT, "\n[STMT] token %.*s", tok.text.len,
-		      tok.text.data);
+		print(WIN_STDOUT, "\n[STMT] identified token '%.*s' as '%s'",
+		      tok.text.len, tok.text.data, getTokenName(tok.type));
 	}
 	return result;
 }
