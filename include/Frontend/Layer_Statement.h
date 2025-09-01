24ad5eea9b8d3dabcd3a584f0bee4ee6e0978fdb
Soham Metha <sohammetha01@gmail.com>
2025-09-01 15:39:57 +0000

    01/09/2025 : 21:09:57

--- include/Frontend/Layer_Statement.h
+++ include/Frontend/Layer_Statement.h
@@ -3,27 +3,6 @@
 #include <Frontend/Layer_Line.h>
 #include <Utils/strings.h>
 
-enum TokenType {
-	TOKEN_TYPE_STR,
-	TOKEN_TYPE_CHAR,
-	TOKEN_TYPE_NUMBER,
-	TOKEN_TYPE_NAME,
-	TOKEN_TYPE_OPEN_PAREN,
-	TOKEN_TYPE_CLOSING_PAREN,
-	TOKEN_TYPE_OPEN_CURLY,
-	TOKEN_TYPE_CLOSING_CURLY,
-	TOKEN_TYPE_STATEMENT_END,
-	TOKEN_TYPE_FUNC,
-	TOKEN_TYPE_COMMA
-};
-
-typedef struct Token Token;
-typedef enum TokenType TokenType;
-struct Token {
-	TokenType type;
-	String text;
-};
-
 Line_View getNextStmt(String line);
 
 #endif
