2109b3f6dc818a403edd47742c2bdd5c904a4ab2
Soham Metha <sohammetha01@gmail.com>
2025-09-01 13:39:22 +0000

    01/09/2025 : 19:09:22

--- include/Frontend/Layer_Xpression.h
+++ include/Frontend/Layer_Xpression.h
@@ -2,6 +2,16 @@
 #define EXPR_LAYER_FRONTEND
 #include <Utils/strings.h>
 
+enum TokenType {
+	TOKEN_TYPE_STR,
+	TOKEN_TYPE_CHAR,
+	TOKEN_TYPE_NUMBER,
+	TOKEN_TYPE_NAME,
+	TOKEN_TYPE_OPEN_PAREN,
+	TOKEN_TYPE_CLOSING_PAREN,
+	TOKEN_TYPE_COMMA
+};
+
 void getNextToken(String line);
 
 #endif
