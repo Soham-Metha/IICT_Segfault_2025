2da4dd8caa6f460326b7f2ab4101fab205a2d275
Soham Metha <sohammetha01@gmail.com>
2025-09-01 13:33:49 +0000

    01/09/2025 : 19:03:49

--- src/Frontend/Layer_Statement.c
+++ src/Frontend/Layer_Statement.c
@@ -1,6 +1,21 @@
 #include <Frontend/Layer_Statement.h>
+#include <Wrapper/IO.h>
 
-void JUST_IGNORE_WARNING()
+Line_View getNextStmt(String line)
 {
-	;
+	Line_View result = { 0 };
+	if (starts_with(line, STR("#"))) {
+		split_str_by_len(&line, 1);
+		result.type = LINE_TYPE_DIRECTIVE;
+		result.value.as_Directive.name =
+			trim(split_str_by_delim(&line, ' '));
+		result.value.as_Directive.body = trim(line);
+		print(WIN_STDOUT,
+		      "\n[STMT] Identified Directive: '%.*s', body: '%.*s'",
+		      result.value.as_Directive.name.len,
+		      result.value.as_Directive.name.data,
+		      result.value.as_Directive.body.len,
+		      result.value.as_Directive.body.data);
+	}
+	return result;
 }
