9db0de1e533d3f9ae821da8f6292c455ee511317
Soham Metha <sohammetha01@gmail.com>
2025-09-01 13:55:08 +0000

    01/09/2025 : 19:25:08

--- include/Frontend/Layer_Xpression.h
+++ include/Frontend/Layer_Xpression.h
@@ -1,25 +1,7 @@
 #ifndef EXPR_LAYER_FRONTEND
 #define EXPR_LAYER_FRONTEND
 #include <Utils/strings.h>
-
-enum TokenType {
-	TOKEN_TYPE_STR,
-	TOKEN_TYPE_CHAR,
-	TOKEN_TYPE_NUMBER,
-	TOKEN_TYPE_NAME,
-	TOKEN_TYPE_OPEN_PAREN,
-	TOKEN_TYPE_CLOSING_PAREN,
-	TOKEN_TYPE_OPEN_CURLY,
-	TOKEN_TYPE_CLOSING_CURLY,
-	TOKEN_TYPE_FUNC,
-	TOKEN_TYPE_COMMA
-};
-
-typedef struct Token Token;
-struct Token {
-	TokenType type;
-	String text;
-};
+#include <Layer_Statement.h>
 
 void getNextToken(String *line);
 
