182af4a5493661c3fd6e8a5afd1497daaf800497
Soham Metha <sohammetha01@gmail.com>
2025-09-01 08:45:18 +0000

    01/09/2025 : 14:15:18

--- include/Backend/Layer_File.h
+++ include/Backend/Layer_File.h
@@ -1,5 +1,10 @@
 #ifndef FILE_LAYER_BACKEND
 #define FILE_LAYER_BACKEND
+#include <Utils/strings.h>
+
+typedef struct File_View {
+	String contents;
+} File_View;
 
 void readFile();
 void generateAST();
