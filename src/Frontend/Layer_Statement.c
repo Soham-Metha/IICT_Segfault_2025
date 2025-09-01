50a8ee50832d6bf9a8655234c6472eaf8214cd16
Soham Metha <sohammetha01@gmail.com>
2025-09-01 16:24:34 +0000

    01/09/2025 : 21:54:34

--- src/Frontend/Layer_Statement.c
+++ src/Frontend/Layer_Statement.c
@@ -26,8 +26,8 @@ Stmt getNextStmt(String line)
 
 		result.type = STMT_LIT_CHAR;
 		result.value.as_char = tok.text.data[0];
-		print(WIN_STDOUT, "\n[STMT] identified '%c' as a char literal",
-		      tok.text.data[0]);
+		// print(WIN_STDOUT, "\n[STMT] identified '%c' as a char literal",
+		//       tok.text.data[0]);
 
 		break;
 
