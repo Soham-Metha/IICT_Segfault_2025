69d68e3d87cdf7bae125b0e4fcffafb214d011b2
Soham Metha <sohammetha01@gmail.com>
2025-09-03 20:54:13 +0000

    04/09/2025 : 02:24:13

--- src/Frontend/Layer_Line.c
+++ src/Frontend/Layer_Line.c
@@ -73,7 +73,7 @@ bool line_parse_next(CodeBlock *blk, File_Context* context)
 			Stmt next = stmt_fetch_next(ctx);
 			statement.value.as_var.defn_val = &next;
 			if (next.type==STMT_BLOCK_START) {
-				statement.value.as_block = codeblock_generate(context).begin;
+				next.value.as_block = codeblock_generate(context).begin;
 			}
 			ctx = file_fetch_curr_line(context);
 			log_to_ctx(ctx, LOG_FORMAT "---------------DEFINITION END-----------------", LOG_CTX("[IDENTIFICATION]","[STMT]"));
