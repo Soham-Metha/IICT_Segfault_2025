10100307e007ed24474a12165b1decfce50384ce
Soham Metha <sohammetha01@gmail.com>
2025-09-03 17:52:52 +0000

    03/09/2025 : 23:22:52

--- src/Frontend/Layer_Line.c
+++ src/Frontend/Layer_Line.c
@@ -51,7 +51,7 @@ void line_get_preprocessed_line(Line_Context *ctx)
 
 	ctx->line = trim(processed_line);
 	
-	log_to_ctx(ctx, LOG_FORMAT "After : \"%.*s\"",
+	log_to_ctx(ctx, LOG_FORMAT "After:  \"%.*s\"",
 		LOG_CTX("[PREPROCESSING]", "[LINE]"),
 		Str_Fmt(ctx->line));
 	
