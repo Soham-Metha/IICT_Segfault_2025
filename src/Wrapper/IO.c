c7c1fcc87ba2a2bf380f43ec97350999be49b7bd
Soham Metha <sohammetha01@gmail.com>
2025-09-03 22:23:03 +0000

    04/09/2025 : 03:53:03

--- src/Wrapper/IO.c
+++ src/Wrapper/IO.c
@@ -45,7 +45,7 @@ void log_to_ctx(Line_Context *ctx, const char *str, ...)
 }
 
 int indent = 0;
-char *space = "#";
+char *space = "##############################";
 String get_indent()
 {
 	return (String){ .data = space, .len = indent*8 };
