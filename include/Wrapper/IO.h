26c53f20f857a692347c14b85b3531b2c48656b6
Soham Metha <sohammetha01@gmail.com>
2025-09-03 19:52:53 +0000

    04/09/2025 : 01:22:53

--- include/Wrapper/IO.h
+++ include/Wrapper/IO.h
@@ -3,7 +3,7 @@
 #include <stdarg.h>
 #include <Frontend/Layer_Line.h>
 
-#define LOG_CTX(action, lvl) lvl, get_indent(), action
+#define LOG_CTX(action, lvl) lvl, Str_Fmt(get_indent()), action
 #define LOG_FORMAT "%6s | %.*s %-20s "
 
 enum WindowNames { WIN_STDIN, WIN_STDOUT, WIN_STDERR, WIN_AST, WIN_COUNT };
