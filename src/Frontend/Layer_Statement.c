107e31aaaddad2d75c8b0d4ebd534b213fd8cfe9
Soham Metha <sohammetha01@gmail.com>
2025-09-03 20:22:12 +0000

    04/09/2025 : 01:52:12

--- src/Frontend/Layer_Statement.c
+++ src/Frontend/Layer_Statement.c
@@ -117,10 +117,10 @@ static inline Stmt __TOKEN_TYPE_CLOSING_CURLY(Token tok, Line_Context* ctx)
 	result.type 		 = STMT_BLOCK_END;
 	result.value.as_token= tok;
 
-	update_indent(10);
+	update_indent(1);
 	log_to_ctx(ctx,
 		LOG_FORMAT, LOG_CTX("[BLOCK END]","[STMT]"));
-	update_indent(-10);
+	update_indent(-1);
 
 	discard_cached_token();
 	return result;
