6aab36c59bf518178a54f76714097ee85aee9044
Soham Metha <sohammetha01@gmail.com>
2025-09-03 16:45:50 +0000

    03/09/2025 : 22:15:50

--- src/Frontend/Layer_File.c
+++ src/Frontend/Layer_File.c
@@ -92,6 +92,7 @@ Line_Context *file_fetch_next_line(File_Context *file)
 	curr->line 			 = split_str_by_delim(&file->contents, '\n');
 	curr->line.data[curr->line.len] = '\0';
 	curr->line_start 	 = curr->line.data;
+	curr->line_no		 = file->line_num;
 	file->line_num 		+= 1;
 
 	log_to_ctx(curr, "[LINE] %3u | %.*s ", file->line_num, Str_Fmt(curr->line));
