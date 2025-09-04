36590ad2fc3ac198fa0e63b1a524327d61c20074
Soham Metha <sohammetha01@gmail.com>
2025-09-04 22:28:07 +0000

    05/09/2025 : 03:58:07

--- src/Utils/strings.c
+++ src/Utils/strings.c
@@ -58,11 +58,6 @@ String split_str_by_delim(String *s, char c)
 
 	String res = split_str_by_len(s, i);
 
-	if (i < s->len) { // discard the delim
-		s->len -= 1;
-		s->data += 1;
-	}
-
 	return res;
 }
 
