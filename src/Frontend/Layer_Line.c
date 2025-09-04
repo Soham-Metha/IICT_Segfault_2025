6c04ed1793cbd84ebc29acab4f1ef1b2acf62182
Soham Metha <sohammetha01@gmail.com>
2025-09-04 22:09:20 +0000

    05/09/2025 : 03:39:20

--- src/Frontend/Layer_Line.c
+++ src/Frontend/Layer_Line.c
@@ -46,9 +46,9 @@ void line_get_preprocessed_line(Line_Context *ctx)
 		log_to_ctx(ctx, LOG_FORMAT "\"%.*s\"",
 				LOG_CTX("[AFTER REMOVAL]", "[LINE]"),
 				Str_Fmt(processed_line));
-	}
 
-	ctx->line = processed_line;
+		ctx->line = processed_line;
+	}
 
 	if (ctx->line.len == 0) {
 		log_to_ctx(ctx, LOG_FORMAT "Line is Blank",
