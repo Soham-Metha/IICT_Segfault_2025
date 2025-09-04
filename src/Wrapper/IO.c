00335afb76fc8c69960555efbf08abafc8740577
Soham Metha <sohammetha01@gmail.com>
2025-09-04 22:50:36 +0000

    05/09/2025 : 04:20:36

--- src/Wrapper/IO.c
+++ src/Wrapper/IO.c
@@ -46,7 +46,7 @@ void log_to_ctx(Line_Context *ctx, const char *str, ...)
 	vsnprintf(log, size + 1, str, args);
 
 	ctx->logs[ctx->log_cnt++] = (String){ .data = log, .len = size };
-	printf("\n%3u | %s",ctx->line_no, log+1);
+	printf("%s",ctx->line_no, log);
 
 	va_end(args);
 }
