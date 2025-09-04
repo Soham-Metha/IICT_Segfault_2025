8e70dc06b351c4c11ce0a9403c4dcd1859bcf5f2
Soham Metha <sohammetha01@gmail.com>
2025-09-04 21:57:31 +0000

    05/09/2025 : 03:27:31

--- src/Wrapper/IO.c
+++ src/Wrapper/IO.c
@@ -24,7 +24,7 @@ void print(Line_Context *ctx, int id, const char *str, ...)
 		vfprintf(myIR, str, args);
 	}else {
 		assert(id == WIN_STDERR);
-		printf("%d:%ld",ctx->line_no,(ctx->line.data-ctx->line_start));
+		printf("%s:%d:%ld:",ctx->file_name, ctx->line_no, (ctx->line.data-ctx->line_start));
 		vprintf(str, args);
 	}
 
