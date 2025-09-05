d72f76e13016c016330c0f0a959b6c1ed559dfe0
Soham Metha <sohammetha01@gmail.com>
2025-09-05 16:04:26 +0000

    05/09/2025 : 21:34:26

--- src/Middleend/Layer_File_IR.c
+++ src/Middleend/Layer_File_IR.c
@@ -122,9 +122,7 @@ static void IR_dump_token(Block_Context_IR* ctx)
 	assert(ctx->next->statement.type==STMT_TOKEN);
 	const Token tok = *ctx->next->statement.value.as_token;
 	if (tok.type != TOKEN_TYPE_STATEMENT_END ) {
-	update_indent(1);
 	print(NULL, WIN_IR, IR_FORMAT "PUSH    \"%.*s\"", IR_CTX(), Str_Fmt(tok.text));
-	update_indent(-1);
 	} else {
 		print(NULL, WIN_IR, IR_FORMAT "%.*s Line end reached", IR_CTX(), 
 			Str_Fmt(tok.text));
