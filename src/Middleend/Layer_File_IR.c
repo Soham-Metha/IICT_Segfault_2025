6dc7b3a4de0afeba9d39e8ff65da1031c9e34aee
Soham Metha <sohammetha01@gmail.com>
2025-09-05 02:18:25 +0000

    05/09/2025 : 07:48:25

--- src/Middleend/Layer_File_IR.c
+++ src/Middleend/Layer_File_IR.c
@@ -111,11 +111,12 @@ int IR__STMT_FUNCALL(int id, int *n, int *b, const Funcall *funcall)
         print(NULL,WIN_IR,"SETR _%d [L0]", child1);
         print(NULL,WIN_IR,"SETR len(_%d) [QT]", child1);
         print(NULL,WIN_IR,"INVOK 7");
+        return id;
     }
 
 	for (const FuncallArg *arg = funcall->args; arg != NULL; arg = arg->next) {
 		int childId = IR_dump_statement(&arg->value, n, b);
-        childId;
+        (void)childId;
 		// if (childId >= 0) print(NULL, WIN_IR, "  Expr_%d -> Expr_%d;\n", id, childId);
 	}
 	return id;
