1b950b1437ef6992d439342f458528da4f2a57c2
Soham Metha <sohammetha01@gmail.com>
2025-09-05 00:24:01 +0000

    05/09/2025 : 05:54:01

--- src/Middleend/Layer_File_IR.c
+++ src/Middleend/Layer_File_IR.c
@@ -84,7 +84,7 @@ int IR__STMT_VARIABLE(int id, Var v, int *n, int *b)
 
 	case VAR_DEFN:
 	case VAR_BOTH:
-    print(NULL,WIN_IR, "DUMPING");
+    print(NULL,WIN_IR, "DUMPING %d", v.defn_val->type);
         IR_dump_statement(v.defn_val, n, b);
         print(NULL,WIN_IR, "Done");
 		if (!compare_str(v.type, STR("func"))) {
