4590384843f5217880a1a82c64de81976c121315
Soham Metha <sohammetha01@gmail.com>
2025-09-05 14:09:46 +0000

    05/09/2025 : 19:39:46

--- include/Middleend/Layer_Line.h
+++ include/Middleend/Layer_Line.h
@@ -1,4 +1,14 @@
 #ifndef LINE_LAYER_MIDDLEEND
 #define LINE_LAYER_MIDDLEEND
 
+#include <Middleend/Layer_Statement.h>
+
+typedef struct BlockScope BlockScope;
+
+struct BlockScope {
+    Var_IR var_defs[128];
+    int var_def_cnt;
+    BlockScope *prev;
+};
+
 #endif
