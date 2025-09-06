6950e729421f586a187485a522b7478bcd5e5d29
Soham Metha <sohammetha01@gmail.com>
2025-09-06 16:38:40 +0000

    06/09/2025 : 22:08:40

--- src/Frontend/Layer_Statement.c
+++ src/Frontend/Layer_Statement.c
@@ -140,6 +140,7 @@ Stmt stmt_fetch_next(Line_Context *ctx)
 	case EXPR_TYPE_THEN:
 	case EXPR_TYPE_REPEAT:
 	case EXPR_TYPE_CLOSING_PAREN:
+	case EXPR_TYPE_TOKEN:
 	case EXPR_TYPE_STATEMENT_END: {
 		Stmt result = { 0 };
 		result.type = STMT_TOKEN;
@@ -147,7 +148,6 @@ Stmt stmt_fetch_next(Line_Context *ctx)
 		expr_consume(ctx);
 		return result;
 	}
-	case EXPR_TYPE_TOKEN:
 	case EXPR_TYPE_COLON:
 	case EXPR_TYPE_EQUAL:
 	default:
