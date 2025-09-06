1af1754bd744c0c767d43db8cc5d30d1b6495997
Soham Metha <sohammetha01@gmail.com>
2025-09-06 00:29:33 +0000

    06/09/2025 : 05:59:33

--- src/Frontend/Layer_Line.c
+++ src/Frontend/Layer_Line.c
@@ -14,19 +14,19 @@ Error codeblock_append_stmt(CodeBlock *list, Stmt statement)
 	// so this is just the logic of appending a node to ll
 
 	assert(list);
-	StmtNode *node 		= region_allocate(sizeof(StmtNode));
+	StmtNode *node = region_allocate(sizeof(StmtNode));
 	ERROR_THROW_IF(ERR_RAN_OUT_OF_MEM, (!node));
-	node->statement 	= statement;
-	node->next 			= NULL;
+	node->statement = statement;
+	node->next = NULL;
 
 	if (list->end == NULL) {
 		assert(list->begin == NULL);
-		list->begin 	= node;
-		list->end 		= node;
+		list->begin = node;
+		list->end = node;
 	} else {
 		assert(list->begin != NULL);
 		list->end->next = node;
-		list->end 		= node;
+		list->end = node;
 	}
 	return ERR_OK;
 }
@@ -37,67 +37,72 @@ void line_get_preprocessed_line(Line_Context *ctx)
 
 	size_t index;
 	if (get_index_of(ctx->line, COMMENT_SYMBOL, &index)) {
-		processed_line = trim(split_str_by_delim(&ctx->line, COMMENT_SYMBOL));
+		processed_line =
+			trim(split_str_by_delim(&ctx->line, COMMENT_SYMBOL));
 
 		log_to_ctx(ctx, LOG_FORMAT "\"%%%.*s\" ",
-			LOG_CTX("[COMMENT FOUND]", "[LINE]"),
-			Str_Fmt(ctx->line));
+			   LOG_CTX("[COMMENT FOUND]", "[LINE]"),
+			   Str_Fmt(ctx->line));
 
 		log_to_ctx(ctx, LOG_FORMAT "\"%.*s\"",
-				LOG_CTX("[AFTER REMOVAL]", "[LINE]"),
-				Str_Fmt(processed_line));
+			   LOG_CTX("[AFTER REMOVAL]", "[LINE]"),
+			   Str_Fmt(processed_line));
 
 		ctx->line = processed_line;
 	}
 
 	if (ctx->line.len == 0) {
 		log_to_ctx(ctx, LOG_FORMAT "Line is Blank",
-			LOG_CTX("[PREPROCESS]", "[LINE]"));
+			   LOG_CTX("[PREPROCESS]", "[LINE]"));
 	}
-
 }
 
 int var_decl_level = 0;
-bool line_parse_next(CodeBlock *blk, File_Context* context)
+bool line_parse_next(CodeBlock *blk, File_Context *context)
 {
-	Line_Context* ctx = file_fetch_curr_line(context);
+	Line_Context *ctx = file_fetch_curr_line(context);
 
 	// if statement identifies the start or end of a block, handle it,
 	// the start of the block would be a pointer to that block's linked list
-	// of statements, forming a linked list of linked lists (2D LL) if 
+	// of statements, forming a linked list of linked lists (2D LL) if
 	// there is single layer of nesting, 3D LL for 2 layers of nesting & so on.
 	// the 1st dimension is the global scope, each layer of nesting adds another dimension.
 	while (ctx->line.len > 0) {
 		line_get_preprocessed_line(ctx);
-		Stmt statement		= stmt_fetch_next(ctx);
+		Stmt statement = stmt_fetch_next(ctx);
 
-		switch (statement.type)
-		{
+		switch (statement.type) {
 		case STMT_VAR:
 			if (statement.value.as_var.mode & VAR_DEFN) {
-				var_decl_level+=1;
-				log_to_ctx(ctx, LOG_FORMAT "Defined value: {", LOG_CTX("[DEFINITION START]","[STMT]"));
+				var_decl_level += 1;
+				log_to_ctx(ctx, LOG_FORMAT "Defined value: {",
+					   LOG_CTX("[DEFINITION START]",
+						   "[STMT]"));
 				update_indent(1);
 			}
 			break;
 		case STMT_BLOCK_START:
-			statement.value.as_block = codeblock_generate(context).begin;
+			statement.value.as_block =
+				codeblock_generate(context).begin;
 			break;
 		case STMT_BLOCK_END:
-			if (var_decl_level>0) {
-				var_decl_level-=1;
+			if (var_decl_level > 0) {
+				var_decl_level -= 1;
 				update_indent(-1);
-				log_to_ctx(ctx, LOG_FORMAT " } ", LOG_CTX("[DEFINITION  END]","[STMT]"));
+				log_to_ctx(ctx, LOG_FORMAT " } ",
+					   LOG_CTX("[DEFINITION  END]",
+						   "[STMT]"));
 			}
 			return true;
 		case STMT_MATCH: {
-			PatternMatch *match = malloc(sizeof(*match));
-			match->cond = malloc(sizeof(*match->cond ));
-			match->body = malloc(sizeof(*match->body ));
+			PatternMatch *match = region_allocate(sizeof(*match));
+			match->cond = region_allocate(sizeof(*match->cond));
+			match->body = region_allocate(sizeof(*match->body));
 			Stmt stmt = stmt_fetch_next(ctx);
 			ctx = file_fetch_curr_line(context);
-			if (stmt.type == STMT_VAR && stmt.value.as_var.mode & VAR_DEFN){
-				stmt.value.as_var.mode &= VAR_DECL; 
+			if (stmt.type == STMT_VAR &&
+			    stmt.value.as_var.mode & VAR_DEFN) {
+				stmt.value.as_var.mode &= VAR_DECL;
 				// the variable in question is not being defined,
 				// even though it's followed by an assignment operator
 				// that assignment operator is syntax sugar for the match statement.
@@ -109,15 +114,20 @@ bool line_parse_next(CodeBlock *blk, File_Context* context)
 			break;
 		}
 		case STMT_CONDITIONAL: {
-			statement.value.as_conditional.body = codeblock_generate(context);
+			statement.value.as_conditional.body =
+				codeblock_generate(context);
+
+			(void)codeblock_append_stmt(blk, statement);
 		} break;
 		case STMT_FUNCALL:
 		case STMT_TOKEN:
-		default:	
-			if (var_decl_level>0) {
-				var_decl_level-=1;
+		default:
+			if (var_decl_level > 0) {
+				var_decl_level -= 1;
 				update_indent(-1);
-				log_to_ctx(ctx, LOG_FORMAT " } ", LOG_CTX("[DEFINITION  END]","[STMT]"));
+				log_to_ctx(ctx, LOG_FORMAT " } ",
+					   LOG_CTX("[DEFINITION  END]",
+						   "[STMT]"));
 			}
 			break;
 		}
@@ -132,21 +142,22 @@ bool line_parse_next(CodeBlock *blk, File_Context* context)
 
 // ----------------------------------------------------------- ACTUAL WORK -------------------------------------------------------------------
 
-CodeBlock codeblock_generate(File_Context* file)
+CodeBlock codeblock_generate(File_Context *file)
 {
-	CodeBlock res 	= { 0 };
-	bool block_end 	= false;
+	CodeBlock res = { 0 };
+	bool block_end = false;
 
 	while (file->contents.len > 0) {
-		Line_Context* ctx = file_fetch_next_line(file);
+		Line_Context *ctx = file_fetch_next_line(file);
 		line_get_preprocessed_line(ctx);
 
-		block_end 	= line_parse_next(&res,file);
+		block_end = line_parse_next(&res, file);
 
-		if (block_end) break;
+		if (block_end)
+			break;
 	}
 
-	// TODO 2: edge case not handled, once file reaches eof, all scopes will 
+	// TODO 2: edge case not handled, once file reaches eof, all scopes will
 	// be automatically closed, for example, if the file only contains '{{',
 	// then it opens 2 layers of nesting, without closing it, this should throw
 	// an error or warning, but it will compile as eof will end every scope.
