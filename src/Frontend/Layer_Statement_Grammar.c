08625caef7ee5e71087627cb5218df462ed06108
Soham Metha <sohammetha01@gmail.com>
2025-09-06 22:27:17 +0000

    07/09/2025 : 03:57:17

--- src/Frontend/Layer_Statement_Grammar.c
+++ src/Frontend/Layer_Statement_Grammar.c
@@ -93,6 +93,10 @@ Stmt stmt_fetch_next(Line_Context *ctx)
 	case TOKEN_TYPE_EOL:
 	default:
 		token_consume(ctx);
+
+		log_to_ctx(ctx, LOG_FORMAT "%s %.*s",
+            LOG_CTX("[IDENTIFICATION]", "[STMT]"),
+            token_get_name(tok.type), Str_Fmt(tok.text));
 		print(ctx, WIN_STDERR, "Unexpected token found!");
 		exit(1);
 	}
