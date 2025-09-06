4cfb4b4bd85593a96974f1e14f2370e85cc0f9ca
Soham Metha <sohammetha01@gmail.com>
2025-09-06 21:31:18 +0000

    07/09/2025 : 03:01:18

--- src/Frontend/Layer_Statement_Grammar.c
+++ src/Frontend/Layer_Statement_Grammar.c
@@ -4,6 +4,7 @@
 
 Stmt stmt_fetch_next(Line_Context *ctx)
 {
+	Stmt result = { 0 };
 	assert(ctx);
 	Token tok = token_peek_next(ctx);
 	// log_to_ctx(ctx, LOG_FORMAT "Checking the first token of the statement to identify statement type, found:", LOG_CTX("[IDENTIFY]","[STMT]"));
@@ -11,22 +12,17 @@ Stmt stmt_fetch_next(Line_Context *ctx)
 	case TOKEN_TYPE_NAME:
 		// return __TOKEN_TYPE_NAME(tok, ctx);
 	case TOKEN_TYPE_OPEN_CURLY:
-		// return __TOKEN_TYPE_OPEN_CURLY(tok, ctx);
 	case TOKEN_TYPE_CLOSING_CURLY:
-		// return __TOKEN_TYPE_CLOSING_CURLY(tok, ctx);
 	case TOKEN_TYPE_OPEN_PAREN:
-		// return __TOKEN_TYPE_OPEN_PAREN(tok, ctx);
-
 	case TOKEN_TYPE_NUMBER:
 	case TOKEN_TYPE_CHAR:
 	case TOKEN_TYPE_STR:
 	case TOKEN_TYPE_THEN:
 	case TOKEN_TYPE_REPEAT:
 	case TOKEN_TYPE_STATEMENT_END: {
-		Stmt result = { 0 };
-		// result.type = STMT_TOKEN;
-		// result.as.token = tok;
-		token_consume(ctx);
+		result.type = STMT_EXPR;
+		result.as.expr = expr_parse(ctx);
+		token_expect_next(ctx, TOKEN_TYPE_STATEMENT_END);
 		return result;
 	}
 	case TOKEN_TYPE_CLOSING_PAREN:
