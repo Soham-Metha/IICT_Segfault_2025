45296a3fa806fd90eaab4f58861b3106e4ef9344
Soham Metha <sohammetha01@gmail.com>
2025-09-06 12:13:15 +0000

    06/09/2025 : 17:43:15

--- include/Utils/errors.h
+++ include/Utils/errors.h
@@ -6,23 +6,23 @@ enum ErrorType {
 	ERR_FILE_CANT_OPEN,
 	ERR_FILE_CANT_READ,
 	ERR_FILE_TOO_LARGE,
-    ERR_RAN_OUT_OF_MEM,
-    ERR_AST_RENDER_ERR,
+	ERR_RAN_OUT_OF_MEM,
+	ERR_AST_RENDER_ERR,
 };
 
 typedef enum ErrorType Error;
 
-#define ERROR_CHECK(var, action, cond)          \
-	{                                           \
-		var = cond;                             \
-		if (var != ERR_OK) {                    \
-			action;                             \
-		}                                       \
+#define ERROR_CHECK(var, action, cond) \
+	{                              \
+		var = cond;            \
+		if (var != ERR_OK) {   \
+			action;        \
+		}                      \
 	}
 
-#define ERROR_THROW_IF(err, cond)               \
-	if (cond) {                                 \
-		return err;                             \
+#define ERROR_THROW_IF(err, cond) \
+	if (cond) {               \
+		return err;       \
 	}
 
 #endif
