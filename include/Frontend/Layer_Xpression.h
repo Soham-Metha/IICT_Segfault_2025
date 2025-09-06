c901da96a102a2f2cab1d6c7b4ecc7fef54c5e24
Soham Metha <sohammetha01@gmail.com>
2025-09-06 19:43:50 +0000

    07/09/2025 : 01:13:50

--- include/Frontend/Layer_Xpression.h
+++ include/Frontend/Layer_Xpression.h
@@ -5,14 +5,13 @@
 #include <Frontend/Layer_Tokens.h>
 #include <stdint.h>
 
-enum ExprType {
+typedef enum {
 	EXPR_TYPE_STR,
 	EXPR_TYPE_NUMBER,
 	EXPR_TYPE_FUNCALL,
 	EXPR_TYPE_VAR,
 	EXPR_TYPE_BOOL,
 	EXPR_TYPE_BIN_OPR,
-
 
 	EXPR_TYPE_OPEN_CURLY,
 	EXPR_TYPE_CLOSING_CURLY,
@@ -22,12 +21,31 @@ enum ExprType {
 	EXPR_TYPE_COLON,
 	EXPR_TYPE_EQUAL,
 	EXPR_TYPE_TOKEN,
-};
+} ExprType;
+
+typedef enum {
+	BIN_OPR_PLUS = 0,
+	BIN_OPR_MINUS,
+	BIN_OPR_MULT,
+	BIN_OPR_LT,
+	BIN_OPR_GE,
+	BIN_OPR_NE,
+	BIN_OPR_AND,
+	BIN_OPR_OR,
+	BIN_OPR_EQ,
+} BinOprType;
+
+typedef enum {
+	BIN_OPR_P0 = 0,
+	BIN_OPR_P1 = 1,
+	BIN_OPR_P2 = 2,
+	BIN_OPR_P3 = 3,
+	COUNT_BIN_OPR_PRECEDENCE,
+} BinOprPrec;
 
 typedef struct Funcall Funcall;
 typedef struct FuncallArg FuncallArg;
 typedef struct Expr Expr;
-typedef enum ExprType ExprType;
 typedef union ExprValue ExprValue;
 typedef struct Line_Context Line_Context;
 
