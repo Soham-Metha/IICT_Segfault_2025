a76e48c605954f786ca263621b710612dfc18616
Soham Metha <sohammetha01@gmail.com>
2025-09-06 12:14:38 +0000

    06/09/2025 : 17:44:38

--- src/Middleend/Layer_Statement.c
+++ src/Middleend/Layer_Statement.c
@@ -8,85 +8,88 @@ int var_def_cnt = 0;
 void push_var_def(String name, String type, int id)
 {
 	assert(var_def_cnt < 128);
-	var_defs[var_def_cnt++] = (Var_IR) { .name = name, .type = get_type_from_name(type), .mem_addr = id };
+	var_defs[var_def_cnt++] = (Var_IR){ .name = name,
+					    .type = get_type_from_name(type),
+					    .mem_addr = id };
 }
 
-int get_var_id(String name) {
-    for (int i = var_def_cnt-1; i >= 0; i--)
-    {
-        if (compare_str(name,var_defs[i].name)){
-            return var_defs[i].mem_addr;
-        }
-    }
-    assert(0 && "VAR NOT IN SCOPE");
+int get_var_id(String name)
+{
+	for (int i = var_def_cnt - 1; i >= 0; i--) {
+		if (compare_str(name, var_defs[i].name)) {
+			return var_defs[i].mem_addr;
+		}
+	}
+	assert(0 && "VAR NOT IN SCOPE");
 }
 
-bool check_var_type(int id, String type) {
-    for (int i = var_def_cnt-1; i >= 0; i--)
-    {
-        if (id==var_defs[i].mem_addr){
-            return get_type_from_name(type)==var_defs[i].type;
-        }
-    }
-    assert(0 && "VAR NOT IN SCOPE");
+bool check_var_type(int id, String type)
+{
+	for (int i = var_def_cnt - 1; i >= 0; i--) {
+		if (id == var_defs[i].mem_addr) {
+			return get_type_from_name(type) == var_defs[i].type;
+		}
+	}
+	assert(0 && "VAR NOT IN SCOPE");
 }
 
-void clear_var_defs() {
-    var_def_cnt = 0;
+void clear_var_defs()
+{
+	var_def_cnt = 0;
 }
 
 typedef enum {
-    VAR_TYPE_STR,
-    VAR_TYPE_I64,
-    VAR_TYPE_F64,
-    VAR_TYPE_CHAR,
-    VAR_TYPE_FUNC,
-    VAR_TYPE_STRUCT,
-    VAR_TYPE_COUNT,
+	VAR_TYPE_STR,
+	VAR_TYPE_I64,
+	VAR_TYPE_F64,
+	VAR_TYPE_CHAR,
+	VAR_TYPE_FUNC,
+	VAR_TYPE_STRUCT,
+	VAR_TYPE_COUNT,
 } varType;
 
 int get_type_from_name(String name)
 {
-    if (compare_str(name,STR("func"))){
-        return VAR_TYPE_FUNC;
-    } else if (compare_str(name,STR("struct"))){
-        return VAR_TYPE_STRUCT;
-    } else if (compare_str(name,STR("char"))){
-        return VAR_TYPE_CHAR;
-    } else if (compare_str(name,STR("i64"))){
-        return VAR_TYPE_I64;
-    } else if (compare_str(name,STR("f64"))){
-        return VAR_TYPE_F64;
-    } else if (compare_str(name,STR("str"))){
-        return VAR_TYPE_STR;
-    }
-    assert(0 && "INVALID TYPE!");
+	if (compare_str(name, STR("func"))) {
+		return VAR_TYPE_FUNC;
+	} else if (compare_str(name, STR("struct"))) {
+		return VAR_TYPE_STRUCT;
+	} else if (compare_str(name, STR("char"))) {
+		return VAR_TYPE_CHAR;
+	} else if (compare_str(name, STR("i64"))) {
+		return VAR_TYPE_I64;
+	} else if (compare_str(name, STR("f64"))) {
+		return VAR_TYPE_F64;
+	} else if (compare_str(name, STR("str"))) {
+		return VAR_TYPE_STR;
+	}
+	assert(0 && "INVALID TYPE!");
 }
 
-int get_size_of_type(int type) {
-    switch(type)
-    {
-        case VAR_TYPE_I64:
-            return 8;
-        case VAR_TYPE_F64:
-            return 8;
-        case VAR_TYPE_CHAR:
-            return 1;
-        case VAR_TYPE_STR:
-        case VAR_TYPE_FUNC:
-        case VAR_TYPE_STRUCT:
-        case VAR_TYPE_COUNT:
-        default:
-            return 0;
-    }
+int get_size_of_type(int type)
+{
+	switch (type) {
+	case VAR_TYPE_I64:
+		return 8;
+	case VAR_TYPE_F64:
+		return 8;
+	case VAR_TYPE_CHAR:
+		return 1;
+	case VAR_TYPE_STR:
+	case VAR_TYPE_FUNC:
+	case VAR_TYPE_STRUCT:
+	case VAR_TYPE_COUNT:
+	default:
+		return 0;
+	}
 }
 
-int get_size_from_id(int id) {
-    for (int i = var_def_cnt-1; i >= 0; i--)
-    {
-        if (id==var_defs[i].mem_addr){
-            return get_size_of_type(var_defs[i].type);
-        }
-    }
-    assert(0 && "INVALID TYPE!");
+int get_size_from_id(int id)
+{
+	for (int i = var_def_cnt - 1; i >= 0; i--) {
+		if (id == var_defs[i].mem_addr) {
+			return get_size_of_type(var_defs[i].type);
+		}
+	}
+	assert(0 && "INVALID TYPE!");
 }
