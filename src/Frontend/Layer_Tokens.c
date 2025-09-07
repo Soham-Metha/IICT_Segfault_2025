9732361676b7c8f3c69cb89d0282afe8d4731a7d
Soham Metha <sohammetha01@gmail.com>
2025-09-07 19:54:34 +0000

    08/09/2025 : 01:24:34

--- src/Frontend/Layer_Tokens.c
+++ src/Frontend/Layer_Tokens.c
@@ -3,9 +3,9 @@
 #include <ctype.h>
 #include <assert.h>
 #include <stdlib.h>
-
-Token tok_cache;
-bool cachedToken = false;
+#define MAX_CACHED_TOKENS 2
+Token tok_cache[MAX_CACHED_TOKENS];
+int cachedCnt = 0;
 
 static bool isName(char x)
 {
@@ -86,130 +86,144 @@ Token token_expect_next(Line_Context *ctx, TokenType expected)
 Token token_peek_next(Line_Context *ctx)
 {
 	String *line = &ctx->line;
-	if (cachedToken)
-		return tok_cache;
-	Token token = { 0 };
-	(*line) = trim(*line);
+	while (cachedCnt < MAX_CACHED_TOKENS) {
+		Token token = { 0 };
+		(*line) = trim(*line);
 
-	if (line->len == 0) {
-		token.type = TOKEN_TYPE_EOL;
-		return token;
+		if (line->len == 0 && cachedCnt > 0) {
+			break;
+		} else if (line->len == 0) {
+			assert(0 && "reached End of line!");
+		}
+
+		switch (line->data[0]) {
+		case '(': {
+			token.type = TOKEN_TYPE_OPEN_PAREN;
+			token.text = split_str_by_len(line, 1);
+		} break;
+
+		case ';': {
+			token.type = TOKEN_TYPE_STATEMENT_END;
+			token.text = split_str_by_len(line, 1);
+		} break;
+
+		case ')': {
+			token.type = TOKEN_TYPE_CLOSING_PAREN;
+			token.text = split_str_by_len(line, 1);
+		} break;
+
+		case '{': {
+			token.type = TOKEN_TYPE_OPEN_CURLY;
+			token.text = split_str_by_len(line, 1);
+		} break;
+
+		case '}': {
+			token.type = TOKEN_TYPE_CLOSING_CURLY;
+			token.text = split_str_by_len(line, 1);
+		} break;
+
+		case ',': {
+			token.type = TOKEN_TYPE_COMMA;
+			token.text = split_str_by_len(line, 1);
+		} break;
+
+		case ':': {
+			token.type = TOKEN_TYPE_COLON;
+			token.text = split_str_by_len(line, 1);
+		} break;
+
+		case '=': {
+			token.type = TOKEN_TYPE_EQUAL;
+			token.text = split_str_by_len(line, 1);
+		} break;
+
+		case '"': {
+			split_str_by_len(line, 1); // discard opening "
+
+			token.type = TOKEN_TYPE_STR;
+			size_t index = 0;
+			if (!get_index_of(*line, '"', &index)) {
+				print(ctx, WIN_STDERR,
+				      "ERROR: Could not find closing \"\n");
+				exit(1);
+			}
+			token.text = split_str_by_len(line, index);
+
+			split_str_by_len(line, 1); // discard closing "
+		} break;
+
+		case '\'': {
+			split_str_by_len(line, 1); // discard opening '
+
+			token.type = TOKEN_TYPE_CHAR;
+			size_t index = 0;
+			if (!get_index_of(*line, '\'', &index)) {
+				print(ctx, WIN_STDERR,
+				      "ERROR: Could not find closing \'\n");
+				exit(1);
+			}
+			token.text = split_str_by_len(line, index);
+
+			split_str_by_len(line, 1); // discard closing '
+		} break;
+
+		default: {
+			if (starts_with(*line, STR("->"))) {
+				token.type = TOKEN_TYPE_THEN;
+				token.text = split_str_by_len(line, 2);
+			} else if (starts_with(*line, STR("then"))) {
+				token.type = TOKEN_TYPE_THEN;
+				token.text = split_str_by_len(line, 4);
+			} else if (starts_with(*line, STR("<->"))) {
+				token.type = TOKEN_TYPE_REPEAT;
+				token.text = split_str_by_len(line, 3);
+			} else if (starts_with(*line, STR("repeat"))) {
+				token.type = TOKEN_TYPE_REPEAT;
+				token.text = split_str_by_len(line, 6);
+			} else if (isalpha(line->data[0])) {
+				token.type = TOKEN_TYPE_NAME;
+				token.text =
+					split_str_by_condition(line, isName);
+			} else if (isdigit(line->data[0]) ||
+				   line->data[0] == '-') {
+				token.type = TOKEN_TYPE_NUMBER;
+				token.text =
+					split_str_by_condition(line, isNumber);
+			} else {
+				print(ctx, WIN_STDERR,
+				      "Unknown token starting with '%c'\n",
+				      line->data[0]);
+				exit(1);
+			}
+		}
+		}
+		tok_cache[cachedCnt] = token;
+		cachedCnt++;
 	}
 
-	switch (line->data[0]) {
-	case '(': {
-		token.type = TOKEN_TYPE_OPEN_PAREN;
-		token.text = split_str_by_len(line, 1);
-	} break;
-
-	case ';': {
-		token.type = TOKEN_TYPE_STATEMENT_END;
-		token.text = split_str_by_len(line, 1);
-	} break;
-
-	case ')': {
-		token.type = TOKEN_TYPE_CLOSING_PAREN;
-		token.text = split_str_by_len(line, 1);
-	} break;
-
-	case '{': {
-		token.type = TOKEN_TYPE_OPEN_CURLY;
-		token.text = split_str_by_len(line, 1);
-	} break;
-
-	case '}': {
-		token.type = TOKEN_TYPE_CLOSING_CURLY;
-		token.text = split_str_by_len(line, 1);
-	} break;
-
-	case ',': {
-		token.type = TOKEN_TYPE_COMMA;
-		token.text = split_str_by_len(line, 1);
-	} break;
-
-	case ':': {
-		token.type = TOKEN_TYPE_COLON;
-		token.text = split_str_by_len(line, 1);
-	} break;
-
-	case '=': {
-		token.type = TOKEN_TYPE_EQUAL;
-		token.text = split_str_by_len(line, 1);
-	} break;
-
-	case '"': {
-		split_str_by_len(line, 1); // discard opening "
-
-		token.type = TOKEN_TYPE_STR;
-		size_t index = 0;
-		if (!get_index_of(*line, '"', &index)) {
-			print(ctx, WIN_STDERR,
-			      "ERROR: Could not find closing \"\n");
-			exit(1);
-		}
-		token.text = split_str_by_len(line, index);
-
-		split_str_by_len(line, 1); // discard closing "
-	} break;
-
-	case '\'': {
-		split_str_by_len(line, 1); // discard opening '
-
-		token.type = TOKEN_TYPE_CHAR;
-		size_t index = 0;
-		if (!get_index_of(*line, '\'', &index)) {
-			print(ctx, WIN_STDERR,
-			      "ERROR: Could not find closing \'\n");
-			exit(1);
-		}
-		token.text = split_str_by_len(line, index);
-
-		split_str_by_len(line, 1); // discard closing '
-	} break;
-
-	default: {
-		if (starts_with(*line, STR("->"))) {
-			token.type = TOKEN_TYPE_THEN;
-			token.text = split_str_by_len(line, 2);
-		} else if (starts_with(*line, STR("then"))) {
-			token.type = TOKEN_TYPE_THEN;
-			token.text = split_str_by_len(line, 4);
-		} else if (starts_with(*line, STR("<->"))) {
-			token.type = TOKEN_TYPE_REPEAT;
-			token.text = split_str_by_len(line, 3);
-		} else if (starts_with(*line, STR("repeat"))) {
-			token.type = TOKEN_TYPE_REPEAT;
-			token.text = split_str_by_len(line, 6);
-		} else if (isalpha(line->data[0])) {
-			token.type = TOKEN_TYPE_NAME;
-			token.text = split_str_by_condition(line, isName);
-		} else if (isdigit(line->data[0]) || line->data[0] == '-') {
-			token.type = TOKEN_TYPE_NUMBER;
-			token.text = split_str_by_condition(line, isNumber);
-		} else {
-			print(ctx, WIN_STDERR,
-			      "Unknown token starting with '%c'\n",
-			      line->data[0]);
-			exit(1);
-		}
-	}
-	}
-	tok_cache = token;
-	cachedToken = true;
-	return token;
+	return tok_cache[0];
 }
 
 bool token_consume(Line_Context *ctx)
 {
 	(void)ctx;
-	if (cachedToken) {
+	if (cachedCnt > 0) {
 		update_indent(1);
 		log_to_ctx(ctx, LOG_FORMAT "<%s '%.*s'>", LOG_CTX("", "[TOKN}"),
-			   token_get_name(tok_cache.type),
-			   Str_Fmt(tok_cache.text));
+			   token_get_name(tok_cache[0].type),
+			   Str_Fmt(tok_cache[0].text));
 		update_indent(-1);
-		cachedToken = false;
+		for (size_t i = 0; i < cachedCnt; i++) {
+			tok_cache[i] = tok_cache[i + 1];
+		}
+		cachedCnt--;
 		return true;
 	}
 	return false;
+}
+Token token_peek_next_next(Line_Context *ctx)
+{
+	(void)token_peek_next(ctx);
+	assert(cachedCnt > 1);
+	return tok_cache[1];
 }
