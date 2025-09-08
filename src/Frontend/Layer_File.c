eb8998cb10442f3eeb9164b9fafa5c84a59eb921
Soham Metha <sohammetha01@gmail.com>
2025-09-08 06:25:51 +0000

    08/09/2025 : 11:55:51

--- src/Frontend/Layer_File.c
+++ src/Frontend/Layer_File.c
@@ -79,7 +79,7 @@ Error file_read(const char *file_path, File_Context *file)
 	CodeBlock global = codeblock_generate(file);
 	(void)global;
 	IR_generate(&global);
-	// AST_generate(&global, true);
+	AST_generate(&global, true);
 
 cleanup:
 	if (file_ptr) {
