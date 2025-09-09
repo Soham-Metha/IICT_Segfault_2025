cbf8d09c1af63e6c237167ac264bbd67a745a70c
Soham Metha <sohammetha01@gmail.com>
2025-09-09 19:00:29 +0000

    10/09/2025 : 00:30:29

--- src/Middleend/Layer_File_IR.c
+++ src/Middleend/Layer_File_IR.c
@@ -246,7 +246,7 @@ varType IR__STMT_FUNCALL(Block_Context_IR *ctx, const Funcall *funcall)
 	if (compare_str(funcall->name, STR("write"))) {
 		for (const FuncallArg *arg = funcall->args; arg != NULL;
 		     arg = arg->next) {
-			IR_dump_expr(ctx, funcall->args->expr);
+			assert(IR_dump_expr(ctx, funcall->args->expr)==VAR_TYPE_STR);
 			print_IR(IR_FORMAT("CALL    write_str", "none"));
 		}
 		return VAR_TYPE_FUNC;
