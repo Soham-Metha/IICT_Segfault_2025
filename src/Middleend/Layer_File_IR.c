ec3e04c25a3928592f36e470bee09d9a184230ad
Soham Metha <sohammetha01@gmail.com>
2025-09-09 18:59:29 +0000

    10/09/2025 : 00:29:29

--- src/Middleend/Layer_File_IR.c
+++ src/Middleend/Layer_File_IR.c
@@ -356,7 +356,7 @@ static varType IR_dump_expr(Block_Context_IR *ctx, Expr expr)
 		assert(bin_opr_inst_LUT[l][expr.as.bin_opr->type].allowed);
 		print_IR(IR_FORMAT("%s",
 				   bin_opr_inst_LUT[l][expr.as.bin_opr->type].inst));
-		return l;
+		return bin_opr_inst_LUT[l][expr.as.bin_opr->type].ret;
 
 	case EXPR_TYPE_THEN:
 	case EXPR_TYPE_REPEAT:
