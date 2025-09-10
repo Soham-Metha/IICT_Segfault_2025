9be7d6279f6955148d25f128f5ade712fc99ab4a
Soham Metha <sohammetha01@gmail.com>
2025-09-10 18:51:15 +0000

    11/09/2025 : 00:21:15

--- src/Middleend/Layer_Statement.c
+++ src/Middleend/Layer_Statement.c
@@ -19,7 +19,7 @@ static TypeDetailsLUT typeDetails[VAR_TYPE_CNT] = {
 };
 
 void push_var_def(Block_Context_IR *ctx, String name, String type, int id,
-		  TypeList list)
+		  TypeList *list)
 {
 	assert(!compare_str(type, STR("void")));
 	assert(ctx->var_def_cnt < 128);
