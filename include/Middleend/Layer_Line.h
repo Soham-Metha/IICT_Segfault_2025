fb2f168ed028a72480cc83d1fac8ccb45e3b820d
Soham Metha <sohammetha01@gmail.com>
2025-09-11 22:39:16 +0530

    formatting

--- include/Middleend/Layer_Line.h
+++ include/Middleend/Layer_Line.h
@@ -1,27 +1,27 @@
 #ifndef LINE_LAYER_MIDDLEEND
 #define LINE_LAYER_MIDDLEEND
 
-#include <Middleend/Layer_Statement.h>
 #include <Frontend/Layer_Statement.h>
+#include <Middleend/Layer_Statement.h>
 
 typedef struct Block_Context_IR Block_Context_IR;
 
 typedef struct Var_IR {
-	String name;
-	int type;
-	int mem_addr;
-	bool has_def;
-	TypeList *list;
+    String name;
+    int type;
+    int mem_addr;
+    bool has_def;
+    TypeList* list;
 } Var_IR;
 
 struct Block_Context_IR {
-	int b;
-	int n;
-	Var_IR var_defs[128];
-	int var_def_cnt;
+    int b;
+    int n;
+    Var_IR var_defs[128];
+    int var_def_cnt;
 
-	StmtNode *next;
-	Block_Context_IR *prev;
+    StmtNode* next;
+    Block_Context_IR* prev;
 };
 
 // void IR_dump_expr(Expr tok, bool asVal);
